ae3d2dc2d518ccd4b6a4368bbfcf969dd845fcf6
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-04-25 19:00:37 +0300

    create custom table view cell

---
+++ DiscoverSaintPetersburgApp/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  DiscoverSaintPetersburgApp
+//
+//  Created by MikhaiL on 25.04.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var locationImage: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+}
